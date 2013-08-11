a9de3abf89c12d3de24e6f63b8272a0d3633889e
SailingYang <x_ig@qq.com>
2013-08-11 18:38:27 +0800

    7.3 ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      redirect_to @user # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :salt, :encrypted_password)
+  end  
 end
